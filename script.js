51bcfa16ba7f2768ead0766f6efe70a1d9ea9d84
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-29 16:20:15 -0700

    get data

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert('hiii');
+let dataset = [];
+const padding = 60;
+let w = 500,
+  h = 500,
+  xScale = 1,
+  yScale = 1,
+  startDate,
+  endDate;
+
+// const d = document.querySelector('#test-suite-selector');
+
+async function getData() {
+  const data = await fetch(
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+  );
+  const res = await data.json();
+  console.log(res);
+  const n = res.length;
+
+  loadPage();
+}
+
+getData();
+
+function loadPage() {}
