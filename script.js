5bf6b8824db534681a1a7807b52fa85028283f39
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-29 16:23:27 -0700

    add svg

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const padding = 60;
 let w = 500,
   h = 500,
   xScale = 1,
-  yScale = 1,
-  startDate,
-  endDate;
+  yScale = 1;
 
 // const d = document.querySelector('#test-suite-selector');
 
@@ -22,4 +20,11 @@ async function getData() {
 
 getData();
 
-function loadPage() {}
+function loadPage() {
+  const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .attr('id', 'title');
+}
