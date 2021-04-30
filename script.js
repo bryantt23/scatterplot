0e509857be031b8c3aac82772efb132488b108b3
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-04-29 17:22:39 -0700

    can see y axis

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 let dataset = [];
 const padding = 60;
-let w = 500,
-  h = 500,
+let w = 500 - padding,
+  h = 500 - padding,
   xScale = 1,
-  yScale = 1;
-
-// const d = document.querySelector('#test-suite-selector');
+  yScale = 1,
+  startDate = 10000,
+  endDate = 0;
 
 async function getData() {
   const data = await fetch(
     'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
   );
-  const res = await data.json();
-  console.log(res);
-  const n = res.length;
+  dataset = await data.json();
+  console.log(dataset);
+  dataset.forEach(element => {
+    startDate = Math.min(startDate, element['Year']);
+    endDate = Math.max(endDate, element['Year']);
+  });
+  console.log(startDate, endDate);
+  console.log(new Date(startDate + ''));
+  const n = dataset.length;
+  // startDate = dataset[0][0];
+  // endDate = dataset[n - 1][0];
+  xScale = w / dataset.length;
 
   loadPage();
 }
@@ -27,4 +36,47 @@ function loadPage() {
     .attr('width', w)
     .attr('height', h)
     .attr('id', 'title');
+
+  svg
+    .selectAll('dot')
+    .data(dataset)
+    .enter()
+    .append('circle')
+
+    .attr('cx', function (d) {
+      // console.log(d);
+      return d['Year'] * xScale;
+    })
+    .attr('cy', function (d) {
+      return 10;
+    })
+    .attr('r', 1.5)
+    .style('fill', '#69b3a2')
+    .attr('width', xScale)
+    .attr('height', 20);
+  // .attr('class', 'bar')
+  // .style('opacity', 0.5)
+  // .attr('x', (d, i) => {
+  //   console.log(d);
+  //   return 5;
+  //   // return i * xScale;
+  // })
+  // .attr('y', (d, i) => {
+  //   return h - 10;
+  //   // return h - d[1] * yScale;
+  // })
+  // .attr('width', xScale)
+  // .attr('height', (d, i) => d[1] * yScale);
+
+  const yAxisScale = d3
+    .scaleTime()
+    .domain([new Date(startDate + ''), new Date(endDate + '')])
+    .range([0, h]);
+
+  const yAxis = d3.axisLeft().scale(yAxisScale);
+  svg
+    .append('g')
+    .attr('id', 'y-axis')
+    .attr('transform', `translate(${padding}, 0)`)
+    .call(yAxis);
 }
